0e87cc888fa5a05cca91cdaa2badc7c5b551dae9
Kayila <arekuanubis@gmail.com>
2020-01-01 14:31:06 -0500

    Added non-preseed ubuntu 18.04 image to the list of images

--- imageprep/ubuntu-installer/ubuntu.php
+++ imageprep/ubuntu-installer/ubuntu.php
@@ -7,5 +7,14 @@ kernel /images/ubuntu-installer/bionic-updates/amd64/linux
 initrd /images/ubuntu-installer/bionic-updates/amd64/initrd.gz
 imgargs linux auto=true preseed/url=http://pxeserver.fox/preseed/ubuntu-installer/bionic/amd64/server.preseed
 BODY
-    );
+  );
+
+  $entries[] = array(
+    "key" => "bionicamd64",
+    "title" => "Install Ubuntu server Bionic amd64 (No preseed)",
+    "body" => <<<BODY
+kernel /images/ubuntu-installer/bionic-updates/amd64/linux
+initrd /images/ubuntu-installer/bionic-updates/amd64/initrd.gz
+BODY
+  );
 ?>
