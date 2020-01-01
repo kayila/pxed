7191ce718d3a90d2eb43802071aefc4af0ec2e47
Kayila <arekuanubis@gmail.com>
2020-01-01 15:24:28 -0500

    Added dban

---
+++ imageprep/dban/dban.php
@@ -0,0 +1,10 @@
+<?php
+  $entries[] = array(
+    "key" => "dban",
+    "title" => "Run Darik's Boot and Nuke",
+    "body" => <<<BODY
+kernel /images/dban.bzi
+imgargs dban.bzi nuke="dwipe" vga=785
+BODY
+  );
+?>
