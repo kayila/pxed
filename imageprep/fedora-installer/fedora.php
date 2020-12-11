5aef897b14ef51780b77b97c5ecaecc2620de70c
Ellen Marie Dash <me@duckie.co>
2020-12-11 18:50:11 -0500

    Add (untested) support for Fedora 33.

---
+++ imageprep/fedora-installer/fedora.php
@@ -0,0 +1,12 @@
+<?php
+  $fedora_version = "33";
+
+  $entries[] = array(
+    "key" => "fedora${fedora_version}amd64",
+    "title" => "Install Fedora ${fedora_version} amd64",
+    "body" => <<<BODY
+kernel /images/fedora-installer/${fedora_version}/amd64/linux
+initrd /images/fedora-installer/${fedora_version}/amd64/initrd.gz
+BODY
+  );
+?>
