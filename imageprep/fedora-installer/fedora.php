2e608f73106e42af4ea1ed941cc3b77d0b42da52
Ellen Marie Dash <me@duckie.co>
2020-12-11 20:51:06 -0500

    Actually pass kernel arguments to Fedora installer.

--- imageprep/fedora-installer/fedora.php
+++ imageprep/fedora-installer/fedora.php
@@ -7,6 +7,7 @@
     "body" => <<<BODY
 kernel /images/fedora-installer/${fedora_version}/amd64/vmlinuz
 initrd /images/fedora-installer/${fedora_version}/amd64/initrd.img
+imgargs vmlinuz initrd=initrd.img inst.repo=https://dl.fedoraproject.org/pub/fedora/linux/releases/33/Everything/x86_64/os
 BODY
   );
 ?>
