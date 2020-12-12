5c15b2a7d04f36245958ed0aaec54f87098b29f3
Ellen Marie Dash <me@duckie.co>
2020-12-11 20:08:31 -0500

    actually point iPXE at the Fedora kernel.

--- imageprep/fedora-installer/fedora.php
+++ imageprep/fedora-installer/fedora.php
@@ -5,7 +5,7 @@
     "key" => "fedora${fedora_version}amd64",
     "title" => "Install Fedora ${fedora_version} amd64",
     "body" => <<<BODY
-kernel /images/fedora-installer/${fedora_version}/amd64/linux
+kernel /images/fedora-installer/${fedora_version}/amd64/vmlinuz
 initrd /images/fedora-installer/${fedora_version}/amd64/initrd.gz
 BODY
   );
