649258d28e18217d40b0915946f15ee44567f7bb
Ellen Marie Dash <me@duckie.co>
2020-12-11 20:12:53 -0500

    Actually use the correct filename. Again.

--- imageprep/fedora-installer/fedora.php
+++ imageprep/fedora-installer/fedora.php
@@ -6,7 +6,7 @@
     "title" => "Install Fedora ${fedora_version} amd64",
     "body" => <<<BODY
 kernel /images/fedora-installer/${fedora_version}/amd64/vmlinuz
-initrd /images/fedora-installer/${fedora_version}/amd64/initrd.gz
+initrd /images/fedora-installer/${fedora_version}/amd64/initrd.img
 BODY
   );
 ?>
