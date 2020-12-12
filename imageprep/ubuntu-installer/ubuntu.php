0afebd6744e1608abcbe7446d4170ead342a0346
Ellen Marie Dash <me@duckie.co>
2020-12-11 20:52:49 -0500

    Improve naming for Ubuntu 18.04

--- imageprep/ubuntu-installer/ubuntu.php
+++ imageprep/ubuntu-installer/ubuntu.php
@@ -1,7 +1,7 @@
 <?php
   $entries[] = array(
     "key" => "bionicsrvamd64",
-    "title" => "Install Ubuntu server Bionic amd64 (With preseed)",
+    "title" => "Install Ubuntu Server 18.04 LTS (Bionic Beaver) amd64 (With preseed)",
     "body" => <<<BODY
 kernel /images/ubuntu-installer/bionic-updates/amd64/linux
 initrd /images/ubuntu-installer/bionic-updates/amd64/initrd.gz
@@ -11,7 +11,7 @@ BODY
 
   $entries[] = array(
     "key" => "bionicamd64",
-    "title" => "Install Ubuntu server Bionic amd64 (No preseed)",
+    "title" => "Install Ubuntu Server 18.04 LTS (Bionic Beaver) amd64 (No preseed)",
     "body" => <<<BODY
 kernel /images/ubuntu-installer/bionic-updates/amd64/linux
 initrd /images/ubuntu-installer/bionic-updates/amd64/initrd.gz
